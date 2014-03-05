4e316121edf6dd39fedf17651be19813db559505
thecauliflowergirl <kdo@ucsd.edu>
2014-03-05 13:54:00 -0800

    mobile page

--- routes/list.js
+++ routes/list.js
@@ -3,14 +3,14 @@
 
 exports.showlist = function(req, res){
 	var text = {
-		'title' : req.body.title
-		'goal' : req.body.goal
-		'needed' : req.body.needed
+		'title' : req.body.title,
+		'goal' : req.body.goal,
+		'needed' : req.body.needed,
 		'current' : req.body.current
 
 	}
 
 	
 
-	res.render('list', {'list_text', text});
+	res.render('list', {'list_text': text});
 };
